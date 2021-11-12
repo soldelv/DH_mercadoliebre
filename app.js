699b5b4b097e9e00e814b582081600592ecb9b36
soldelv <joanadelvalle@hotmail.com.ar>
2021-11-11 22:10:47 -0300

    subiendo proyecto

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+
+app.listen( process.env.PORT || 3000, function(){
+    console.log('Servidor funcionando en el puerto 300');
+});
+
+app.get('/home', (req,res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
